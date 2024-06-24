bb259eb1b497148b4a11af191dd515fffcd43165
mattmc3 <mattmc3@gmail.com>
2024-06-24 19:58:07 -0400

    WIP

--- antidote-use-omz.plugin.zsh
+++ antidote-use-omz.plugin.zsh
@@ -4,12 +4,13 @@
 
 # References:
 # - https://github.com/ohmyzsh/ohmyzsh/blob/master/oh-my-zsh.sh
+# - https://github.com/zsh-users/zsh/blob/master/Completion/compinit
 
 # Oh-My-Zsh can be a bit of a dependency [Rat King](https://en.wikipedia.org/wiki/Rat_king).
 # But, it has some really useful plugins. This plugin serves to simply untangle those rat
 # tails so that OMZ plugins can be used with the antidote Zsh plugin manager seamlessly.
 
-#region helpers
+#region init and helpers
 0=${(%):-%N}
 
 ##? Check if a name is a command, function, or alias.
@@ -17,8 +18,13 @@ function is-callable {
   (( $+commands[$1] || $+functions[$1] || $+aliases[$1] || $+builtins[$1] ))
 }
 
-# Support additional Zsh hooks.
-(( $+functions[hooks-add-hook] )) || source ${0:A:h}/zsh-hooks.zsh
+##? Check a string for case-insensitive "true" value (1,y,yes,t,true,on).
+function is-true {
+  [[ -n "$1" && "$1:l" == (1|y(es|)|t(rue|)|on) ]]
+}
+
+# Ensure fpath does not contain duplicates.
+typeset -gU fpath
 #endregion
 
 #region Set $ZSH
@@ -56,205 +62,140 @@ if [[ ! -d $ZSH ]]; then
 fi
 #endregion
 
-#region https://github.com/ohmyzsh/ohmyzsh/blob/a87e9c715b2d3249681f9cc8f8d9718030674d50/oh-my-zsh.sh#L53-L68
+#region OMZ core variables
+# https://github.com/ohmyzsh/ohmyzsh/blob/a87e9c715b2d3249681f9cc8f8d9718030674d50/oh-my-zsh.sh#L53-L68
 # Set ZSH_CUSTOM to the path where your custom config files
-# and plugins exists, or else we will use the default custom/
+# and plugins exists, or else we will use the default custom.
 [[ -n "$ZSH_CUSTOM" ]] || ZSH_CUSTOM="$ZSH/custom"
 
 # Set ZSH_CACHE_DIR to the path where cache files should be created
-# or else we will use the default cache/
-[[ -n "$ZSH_CACHE_DIR" ]] || ZSH_CACHE_DIR="$ZSH/cache"
+# or else we will use the default cache.
+[[ -n "$ZSH_CACHE_DIR" ]] || ZSH_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/zsh"
 
-# Make sure $ZSH_CACHE_DIR is writable, otherwise use a directory in $HOME
-if [[ ! -w "$ZSH_CACHE_DIR" ]]; then
-  ZSH_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/oh-my-zsh"
-fi
-
-# Create cache and completions dir and add to $fpath
+# Create cache and completions dir and add to $fpath.
 mkdir -p "$ZSH_CACHE_DIR/completions"
 (( ${fpath[(Ie)"$ZSH_CACHE_DIR/completions"]} )) || fpath=("$ZSH_CACHE_DIR/completions" $fpath)
 #endregion
 
+#region Defer compinit and queue completion commands
+# Define wrapper functions for completion commands so we can queue up calls to them.
+# That way after compinit is called for real, we can play back the queue.
+# TODO: Add more builtin completion functions for queuing if others are discovered.
+typeset -gHa __compcmd_queue=()
+function queued_compcmd compdef {
+  0=${(%):-%N}
+  if (( $# )); then
+    local -a args=("${@[@]}")
+    __compcmd_queue+=("$(typeset -p args); $0 \$args")
+  else
+    __compcmd_queue+=("$0")
+  fi
+}
 
+# Deferred compinit. When it's finally called, the queue of completion commands is
+# processed in order.
+function compinit compinit_deferred {
+  # Unset any completion wrappers.
+  unfunction compinit queued_compcmd compdef &>/dev/null
 
+  # Load the real compinit.
+  unfunction compinit &>/dev/null
+  autoload -Uz compinit && compinit "${@[@]}"
 
+  # Load other stock functions (from $fpath files) called below.
+  autoload -U compaudit zrecompile
 
+  # Process the completion command queue.
+  local cmpcmd
+  for cmpcmd in $__compcmd_queue; do
+    eval $cmpcmd
+  done
+  unset __compcmd_queue
 
+  # Cleanup.
+  unfunction compinit_deferred
+}
 
+# In case the user didn't decide to explicitly call compinit, we attach compinit to the
+# precmd hook so that we ensure completions are intialized at the very end.
+autoload -Uz add-zsh-hook
+function ensure-compinit-during-precmd {
+  # If the shell is interactive, ensure compinit runs before the first command.
+  if [[ -o interactive ]] && (( $+functions[compinit_deferred] )); then
+    run-compinit
+  fi
 
+  # Remove the hook so that it doesn't keep running on every precmd event.
+  add-zsh-hook -d precmd ensure-compinit-during-precmd
+}
+add-zsh-hook precmd ensure-compinit-during-precmd
+#endregion
 
+#region Run compinit
+# Check ZSH_COMPDUMP metadata to see if zcompdump has expired.
+function has-zcompdump-expired {
+  emulate -L zsh
+  setopt local_options
 
+  # Get current metadata.
+  local zsh_compdump_rev="$(git -C "$ZSH" rev-parse HEAD 2>/dev/null)"
+  local zsh_compdump_fpath=($fpath)
 
-# # Reference:
-# # - https://github.com/zsh-users/zsh/blob/master/Completion/compinit
+  # Get previous metadata.
+  typeset -g ZSH_COMPDUMP_REV
+  typeset -ga ZSH_COMPDUMP_FPATH
+  [[ -r $ZSH_CACHE_DIR/zcompdump-metadata.zsh ]] && source $ZSH_CACHE_DIR/zcompdump-metadata.zsh
 
-# # Define wrapper functions for completion commands so we can queue up calls to them.
-# # That way after compinit is called for real, we can play back the queue.
-# # TODO: Add more than compdef if there are others needed.
-# typeset -gHa __compcmd_queue=()
-# function queued_compcmd compdef {
-#   0=${(%):-%N}
-#   if (( $# )); then
-#     local -a args=("${@[@]}")
-#     __compcmd_queue+=("$(typeset -p args); $0 \$args")
-#   else
-#     __compcmd_queue+=("$0")
-#   fi
-# }
+  # Compare to see if zcompdump should be expired.
+  if [[ "$zsh_compdump_rev" != "$ZSH_COMPDUMP_REV" ]] ||
+     [[ "$zsh_compdump_fpath" != "$ZSH_COMPDUMP_FPATH" ]]
+  then
+    ZSH_COMPDUMP_REV=$zsh_compdump_rev
+    ZSH_COMPDUMP_FPATH=($zsh_compdump_fpath)
+    { typeset -p ZSH_COMPDUMP_REV; typeset -p ZSH_COMPDUMP_FPATH } > $ZSH_CACHE_DIR/zcompdump-metadata.zsh
+  else
+    return 1
+  fi
+}
 
-# # Wrap compinit so that when it's finally called the queue of completion commands is
-# # processed in order.
-# typeset -gHa __compinit_args=()
-# function compinit {
-#   __compinit_args=("${@[@]}")
-# }
+# Figure out the SHORT hostname
+if [[ -z "$SHORT_HOST" ]]; then
+  # Figure out the SHORT hostname
+  if [[ "$OSTYPE" = darwin* ]]; then
+    # macOS's $HOST changes with dhcp, etc. Use ComputerName if possible.
+    SHORT_HOST=$(scutil --get ComputerName 2>/dev/null) || SHORT_HOST="${HOST/.*/}"
+  else
+    SHORT_HOST="${HOST/.*/}"
+  fi
+fi
 
-# # Deferred compinit call. When it's finally called, the queue of completion commands is
-# # processed in order.
-# function compinit_deferred {
-#   # Unset any completion wrappers.
-#   unfunction compinit queued_compcmd compdef &>/dev/null
+# Save the location of the current completion dump file.
+if [[ -z "$ZSH_COMPDUMP" ]]; then
+  ZSH_COMPDUMP="$ZSH_CACHE_DIR/zcompdump-${SHORT_HOST}-${ZSH_VERSION}"
+fi
 
-#   # Load the real compinit.
-#   autoload -Uz compinit && compinit "${__compinit_args[@]}"
+function run-compinit {
+  has-zcompdump-expired && command rm -f "$ZSH_COMPDUMP"
 
-#   # Process the completion command queue.
-#   local cmpcmd
-#   for cmpcmd in $__compcmd_queue; do
-#     eval $cmpcmd
-#   done
-#   unset __compcmd_queue
+  if [[ -n "$ZSH_DISABLE_COMPFIX" ]] && ! is-true "$ZSH_DISABLE_COMPFIX"; then
+    source "$ZSH/lib/compfix.zsh"
+    # Load only from secure directories
+    compinit -i -d "$ZSH_COMPDUMP"
+    # If completion insecurities exist, warn the user
+    handle_completion_insecurities &|
+  else
+    # If the user wants it, load from all found directories
+    compinit -u -d "$ZSH_COMPDUMP"
+  fi
 
-#   # Cleanup.
-#   unfunction compinit_deferred
-# }
-
-# # If the user didn't specify they wanted to manually initialize completions,
-# # then attach compinit to the precmd hook so that it happens at the very end.
-# autoload -Uz add-zsh-hook
-# function ensure-compinit-during-precmd {
-#   # If the shell is interactive, ensure compinit runs before the first command.
-#   if [[ -o interactive ]] && (( $+functions[compinit_deferred] )); then
-#     compinit_deferred
-#   fi
-
-#   # Remove the hook so that it doesn't keep running on every precmd event.
-#   add-zsh-hook -d precmd ensure-compinit-during-precmd
-# }
-# add-zsh-hook precmd ensure-compinit-during-precmd
-
-
-
-
-
-
-
-# # Bootstrap.
-# 0=${(%):-%N}
-# zstyle -t ':zephyr:lib:bootstrap' loaded || source ${0:a:h:h:h}/lib/bootstrap.zsh
-# autoload-dir ${0:a:h}/functions
-
-# # 16.2.2 Completion
-# setopt always_to_end        # Move cursor to the end of a completed word.
-# setopt auto_list            # Automatically list choices on ambiguous completion.
-# setopt auto_menu            # Show completion menu on a successive tab press.
-# setopt auto_param_slash     # If completed parameter is a directory, add a trailing slash.
-# setopt complete_in_word     # Complete from both ends of a word.
-# setopt NO_menu_complete     # Do not autoselect the first completion entry.
-
-# # 16.2.3 Expansion and Globbing
-# setopt extended_glob        # Needed for file modification glob modifiers with compinit.
-
-# # 16.2.6 Input/Output
-# setopt path_dirs            # Perform path search even on command names with slashes.
-# setopt NO_flow_control      # Disable start/stop characters in shell editor.
-
-# # Add other upstream completions to $fpath.
-# fpath=(
-#   ${0:a:h}/external/src  # zsh-users/zsh-completions
-#   ${0:a:h}/completions   # starship, git contrib
-#   $fpath
-# )
-
-# # Add completions for keg-only brews when available.
-# if (( $+commands[brew] )); then
-#   brew_prefix=${HOMEBREW_PREFIX:-${HOMEBREW_REPOSITORY:-$commands[brew]:A:h:h}}
-#   # $HOMEBREW_PREFIX defaults to $HOMEBREW_REPOSITORY but is explicitly set to
-#   # /usr/local when $HOMEBREW_REPOSITORY is /usr/local/Homebrew.
-#   # https://github.com/Homebrew/brew/blob/2a850e02d8f2dedcad7164c2f4b95d340a7200bb/bin/brew#L66-L69
-#   [[ $brew_prefix == '/usr/local/Homebrew' ]] && brew_prefix=$brew_prefix:h
-
-#   # Add brew locations to fpath
-#   fpath=(
-#     # Add curl completions from homebrew.
-#     $brew_prefix/opt/curl/share/zsh/site-functions(/N)
-
-#     # Add zsh completions.
-#     $brew_prefix/share/zsh/site-functions(-/FN)
-
-#     $fpath
-#   )
-#   unset brew_prefix
-# fi
-
-# # Add custom completions.
-# fpath=(${ZDOTDIR:-${XDG_CONFIG_HOME:-$HOME/.config}/zsh}/completions(-/FN) $fpath)
-
-# # Let's talk compinit... compinit works by finding _completion files in your fpath. That
-# # means fpath has to be fully populated prior to calling compinit. If you use oh-my-zsh,
-# # if populates fpath and runs compinit prior to loading plugins. This is only
-# # problematic if you expect to be able to add to fpath later. Conversely, if you wait to
-# # run compinit until the end of your config, then functions like compdef aren't
-# # available earlier in your config.
-# #
-# # TLDR; this code handles all those use cases and simply queues calls to compdef and
-# # hooks compinit to precmd for one call only, which happens automatically at the end of
-# # your config. You can override this behavior with zstyles.
-# if zstyle -t ':zephyr:plugin:completion' immediate; then
-#   run-compinit
-# else
-#   # Define compinit placeholder functions (compdef) so we can queue up calls to compdef.
-#   # That way when the real compinit is called, we can execute the queue.
-#   typeset -gHa __zephyr_compdef_queue=()
-#   function compdef {
-#     (( $# )) || return
-#     local compdef_args=("${@[@]}")
-#     __zephyr_compdef_queue+=("$(typeset -p compdef_args)")
-#   }
-
-#   # Wrap compinit temporarily so that when the real compinit call happens, the
-#   # queue of compdef calls is processed.
-#   function compinit {
-#     unfunction compinit compdef &>/dev/null
-#     autoload -Uz compinit && compinit "$@"
-#     local typedef_compdef_args
-#     for typedef_compdef_args in $__zephyr_compdef_queue; do
-#       eval $typedef_compdef_args
-#       compdef "$compdef_args[@]"
-#     done
-#     unset __zephyr_compdef_queue
-#   }
-
-#   # If the user didn't specify they wanted to manually initialize completions,
-#   # then attach compinit to the precmd hook so that it happens at the very end.
-#   if ! zstyle -t ':zephyr:plugin:completion' manual; then
-#     autoload -Uz add-zsh-hook
-#     function run-compinit-precmd {
-#       run-compinit
-#       add-zsh-hook -d precmd run-compinit-precmd
-#     }
-#     add-zsh-hook precmd run-compinit-precmd
-#   fi
-# fi
-
-# # Set the completion style
-# zstyle -s ':zephyr:plugin:completion' compstyle 'zcompstyle' || zcompstyle=zephyr
-# if (( $+functions[compstyle_${zcompstyle}_setup] )); then
-#   compstyle_${zcompstyle}_setup
-# else
-#   compstyleinit && compstyle ${zcompstyle}
-# fi
-# unset zcompstyle
-
-# # Mark this plugin as loaded.
-# zstyle ':zephyr:plugin:completion' loaded 'yes'
+  # Compile zcompdump, if modified, in background to increase startup speed.
+  {
+    if [[ -s "$ZSH_COMPDUMP" && (! -s "${ZSH_COMPDUMP}.zwc" || "$ZSH_COMPDUMP" -nt "${ZSH_COMPDUMP}.zwc") ]]; then
+      if command mkdir "${ZSH_COMPDUMP}.lock" 2>/dev/null; then
+        zrecompile -q -p "$ZSH_COMPDUMP"
+        command rm -rf "${ZSH_COMPDUMP}.zwc.old" "${ZSH_COMPDUMP}.lock" 2>/dev/null
+      fi
+    fi
+  } &!
+}
+#endregion
